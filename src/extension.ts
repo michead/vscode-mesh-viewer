27ae74f90bd598298306a91db9b4e0813005cdd1
michead <countrymetal@live.it>
2017-10-10 23:37:36 +0100

    Fixes to WebGL context initialization

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,7 @@ import * as vscode from 'vscode';
 import fs = require('fs');
 import path = require('path');
 
-function parseObjMesh(data) {
-  return {};
-}
-
 export function activate(context: vscode.ExtensionContext) {
-    vscode.workspace.registerTextDocumentContentProvider('mesh-preview', {
-      provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): string {
-        return fs.readFileSync(path.resolve(__dirname, '../../index.html')).toString('utf-8');
-      }
-    });
-
     const disposable = vscode.commands.registerCommand('extension.openMeshPreview', (selectedItem) => {
       const fsPath = selectedItem.fsPath;
       const previewUri = vscode.Uri.parse(`mesh-preview://${fsPath}`);
@@ -28,14 +18,23 @@ export function activate(context: vscode.ExtensionContext) {
           return;
         }
 
-        let mesh;
-        if (mesh = parseObjMesh(data)) {
-          return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Active, 'Mesh Preview')
-            .then((success) => {
-            }, (reason) => {
-              vscode.window.showErrorMessage(reason);
-            });
-        }
+        vscode.workspace.registerTextDocumentContentProvider('mesh-preview', {
+          provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): string {
+            return fs.readFileSync(path.resolve(__dirname, 'index.html'))
+              .toString('utf-8')
+              .replace('${mesh}', data.toString('utf-8'))
+              .replace(/\${path}/g, __dirname);
+          }
+        });
+
+        vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Active, 'Mesh Preview', {
+          allowScripts: true,
+          allowSvgs: true
+        })
+          .then((success) => {
+          }, (reason) => {
+            vscode.window.showErrorMessage(reason);
+          });
       });
     });
 
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,115 @@
-const canvas = document.getElementById('mesh-preview-root') as HTMLCanvasElement;
-const objData = document.getElementById('mesh-preview-data').nodeValue;
-const vertShader = document.getElementById('vert-shader').nodeValue;
-const fragShader = document.getElementById('frag-shader').nodeValue;
+const canvas  = document.getElementById('mesh-preview-canvas') as HTMLCanvasElement;
+const objData = document.getElementById('mesh-preview-obj').innerText;
+const vert    = document.getElementById('mesh-preview-vert-shader').innerText;
+const frag    = document.getElementById('mesh-preview-frag-shader').innerText;
 
-let gl;
+declare var mat4;
 
-type vec3 = {
-  x: Number;
-  y: Number;
-  z: Number;
+class Mesh {
+  vertices: number[][];
+  faces:    number[][];
 }
 
-type vec4 = vec3 | {
-  w: number;
+let gl: WebGLRenderingContext;
+let shaderProgram;
+let mesh;
+let meshVertexBuffer;
+let modelViewMatUniform;
+let projMatUniform;
+let positionAttributeLocation;
+
+function initGL(): void {
+  gl = canvas.getContext('webgl', { antialias: true });
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0, 0, 0, 1);
 }
 
-type Mesh = {
-  positions: vec4;
-}
-
-function initGL(canvas: HTMLCanvasElement): void {
-  gl = canvas.getContext('webgl');
-  gl.viewportWidth = canvas.width;
-  gl.viewportHeight = canvas.height;
-}
-
-function initShaders(vert: string, frag: string): void {
+function initShaders(): void {
   const vertShader = gl.createShader(gl.VERTEX_SHADER);
   const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
   gl.shaderSource(vertShader, vert);
   gl.shaderSource(fragShader, frag);
 
-  const shaderProgram = gl.createShaderProgram();
-  gl.attachShader(vertShader);
-  gl.attachShader(fragShader);
-  gl.linkProgram(shaderProgram);
+  [vertShader, fragShader].forEach(shader => {
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      throw new Error(`Error encountered while compiling shader: ${gl.getShaderInfoLog(shader)}`);
+    }
+  });
 
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertShader);
+  gl.attachShader(shaderProgram, fragShader);
+  gl.linkProgram(shaderProgram);
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    throw new Error(`Error encountered while linking shaders: ${gl.getProgramInfoLog(shaderProgram)}`);
+  }
   gl.useProgram(shaderProgram);
 }
 
-function parseMesh(meshStr: string): Mesh {
-  const lines = meshStr.split('\n');
+function parseMesh(): void {
+  const lines = objData.split('\n');
+  const vertices = [];
+  const faces = [];
 
   for (const line of lines) {
-    // TODO: Process line
+    const [type, ...rest] = line.trim().split(' ');
+    switch (type) {
+      case 'v':
+      const [x, y, z] = rest.map(n => parseFloat(n));
+      vertices.push([x, y, z]);
+      break;
+      case 'f':
+      const [a, b, c] = rest.map(n => parseInt(n) - 1);
+      faces.push([a, b, c])
+      break;
+    }
   }
 
-  return {
-    positions: {} as any
+  mesh = {
+    vertices,
+    faces
   };
 }
 
-initGL(canvas);
-initShaders(vertShader, fragShader);
+function initBuffers() {
+  const vertices = [];
+  mesh.vertices.forEach((vertex) => {
+    vertices.push(vertex[0]);
+    vertices.push(vertex[1]);
+    vertices.push(vertex[2]);
+  });
+  meshVertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, meshVertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  positionAttributeLocation = gl.getAttribLocation(shaderProgram, "position");
+}
 
-const mesh = parseMesh(objData);
+function initUniforms() {
+  const modelViewMat = mat4.create();
+  const projMat      = mat4.create();
+  mat4.identity(modelViewMat);
+  mat4.translate(modelViewMat, modelViewMat, [0, 0, -5]);
+  mat4.perspective(projMat, 45, canvas.width / canvas.height, 0.1, 100);
+  modelViewMatUniform = gl.getUniformLocation(shaderProgram, "modelViewMat");
+  projMatUniform      = gl.getUniformLocation(shaderProgram, "projMat");
+  gl.uniformMatrix4fv(modelViewMatUniform, false, modelViewMat);
+  gl.uniformMatrix4fv(projMatUniform, false, projMat);
+}
+
+function draw() {
+  requestAnimationFrame(draw);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.bindBuffer(gl.ARRAY_BUFFER, meshVertexBuffer);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, mesh.vertices.length);
+}
+
+initGL();
+initShaders();
+parseMesh();
+initBuffers();
+initUniforms();
+draw();
